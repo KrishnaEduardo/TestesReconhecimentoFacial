ca62a1dac792f98b51e85060489b97ea4c6eda00
Lucas Suchy <suchylima.lucas@gmail.com>
2023-11-30 12:16:51 -0300

    Subindo reocnhecimento facial

---
+++ main_video.py
@@ -0,0 +1,56 @@
+import cv2
+from simple_facerec import SimpleFacerec
+import datetime
+import sqlite3
+import requests
+con = sqlite3.connect("usuarios.db")
+
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images("images/")
+
+# Load Camera
+cap = cv2.VideoCapture(0)
+
+
+def banco(usuario):
+    query1 = f"select usuario, telefone, apikey from usuarios where id = ?"
+    res = con.execute(query1, (usuario,))
+    retorno = res.fetchone()
+    if retorno:
+        query2 = f"select * from registros where created_At < datetime('now', 'localtime', '-600 seconds') and usuario_id = ?"
+        res2 = con.execute(query2, (usuario,))
+        resultado = res2.fetchone()
+        if not resultado:
+            return retorno
+
+
+def requisicao(usuario, telefone, apikey):
+    mensagem = "Ponto Registrado"
+    url = f"https://api.callmebot.com/whatsapp.php?phone={telefone}&text={mensagem}&apikey={apikey}"
+    payload = {}
+    headers = {}
+    response  = requests.request("GET", url, headers=headers, data=payload)
+    print(response)
+
+
+while True:
+    ret, frame = cap.read()
+
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        dados = banco(face_names[0])
+        requisicao(dados[0],dados[1], dados[2])
+        cv2.putText(frame, dados[0],(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ sql.sql
@@ -0,0 +1,36 @@
+drop table usuarios;
+CREATE TABLE `usuarios` (
+  `id` integer primary key AUTOINCREMENT ,
+  `usuario` varchar(255) default NULL,
+  `telefone` varchar(100) default NULL,
+  `apikey` varchar(255)
+
+);
+
+CREATE TABLE `registros` (
+  `id` integer primary key AUTOINCREMENT ,
+  `usuario_id` varchar(255) default NULL,
+  `registro` datetime default NULL,
+  `created_at` datetime default null
+);
+
+INSERT INTO `usuarios` (`usuario`,`telefone`,`apikey`)
+VALUES
+  ("Lucas Suchy","5551998742477","5376026");
+
+select usuario, telefone, apikey from usuarios where usuario = 'Lucas Suchy';
+
+update usuarios set apikey = 5376026 where id = 1;
+
+
+truncate registros;
+INSERT INTO `registros` (`usuario_id`,`registro`,`created_at`)
+VALUES
+  ("1",datetime('now', 'localtime'),datetime('now', 'localtime'));
+
+SELECT datetime('now', 'localtime')
+;
+(col, 'PDT')
+
+;
+select * from registros where created_At < datetime('now', 'localtime', '-600 seconds') and usuario_id = ?
